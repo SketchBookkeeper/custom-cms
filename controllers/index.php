8bdb564b13e498e546380a2270bcd15eff4e334e
Paul Allen <p.allen.art@gmail.com>
2018-06-17 23:50:12 -0600

    clean up router.

--- controllers/index.php
+++ controllers/index.php
@@ -1,10 +1,12 @@
 <?php
 require 'core/Task.php';
 
-$tasks = $database->SelectAll('todos');
+$tasks = $app['database']->SelectAll('todos');
 
 $tasks = array_map(function ($task) {
 
 	return new Task($task->description);
 
 }, $tasks);
+
+require 'views/index.view.php';
